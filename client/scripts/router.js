148025cc36c381b7e71e912e5487954f49af1d2f
CsataiBalint <balint@csatai.hu>
2015-11-18 02:26:28 +0100

    Router beforeaction modify

--- client/scripts/router.js
+++ client/scripts/router.js
@@ -6,19 +6,6 @@ Router.configure({
   layoutTemplate: 'layout'
 });
 
-/**
- * Authentication beforeAction.
- * Prevents access to the user details page if user is not logged in.
- */
-Router.onBeforeAction(function () {
-  if (!Meteor.userId()) {
-    this.redirect('events');
-  } else {
-    this.next();
-  }
-});
-
-
 
 Router.route('/rangList', {
   name:     'rangList',
